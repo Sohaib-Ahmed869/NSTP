c329d7cb26192b15530242542772d8000a90fff9
Musa <musaharoon.2003@gmail.com>
2024-08-20 10:16:55 +0500

    navbar selected option

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,6 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import nstplogo from "../Assets/nstp.png";
 const Navbar = () => {
+  const [activeLink, setActiveLink] = useState(window.location.pathname);
+
+  useEffect(() => {
+    setActiveLink(window.location.pathname);
+  }, []);
+
+  const handleClick = (link) => {
+    window.location.href = link;
+  };
+
   return (
     <div className="z-10 bg-black text-white p-7 rounded-3xl mt-5 mb-5 w-4/5 border-4 border-gray-300 flex justify-between items-center fixed">
       <div className="flex space-x-5 items-center">
@@ -10,51 +20,73 @@ const Navbar = () => {
       <div className="flex space-x-5 items-center">
         <a
           href="#"
-          className="hover:text-green-500 cursor-pointer"
-          onClick={() => (window.location.href = "/")}
+          className={`cursor-pointer ${
+            activeLink === "/" ? "text-green-500" : "hover:text-green-500"
+          }`}
+          onClick={() => handleClick("/")}
         >
           Home
         </a>
         <a
-          className="hover:text-green-500 cursor-pointer"
-          onClick={() => (window.location.href = "/about")}
+          className={`cursor-pointer ${
+            activeLink === "/about" ? "text-green-500" : "hover:text-green-500"
+          }`}
+          onClick={() => handleClick("/about")}
         >
           About
         </a>
         <a
-          className="hover:text-green-500 cursor-pointer"
-          onClick={() => (window.location.href = "/innovate")}
+          className={`cursor-pointer ${
+            activeLink === "/innovate"
+              ? "text-green-500"
+              : "hover:text-green-500"
+          }`}
+          onClick={() => handleClick("/innovate")}
         >
           Innovate
         </a>
         <a
           href="#"
-          className="hover:text-green-500 cursor-pointer"
-          onClick={() => (window.location.href = "/sectors")}
+          className={`cursor-pointer ${
+            activeLink === "/sectors"
+              ? "text-green-500"
+              : "hover:text-green-500"
+          }`}
+          onClick={() => handleClick("/sectors")}
         >
           Sectors
         </a>
         <a
-          className="hover:text-green-500 cursor-pointer"
-          onClick={() => (window.location.href = "/events")}
+          className={`cursor-pointer ${
+            activeLink === "/events" ? "text-green-500" : "hover:text-green-500"
+          }`}
+          onClick={() => handleClick("/events")}
         >
           Events
         </a>
         <a
-          className="hover:text-green-500 cursor-pointer"
-          onClick={() => (window.location.href = "/partnerships")}
+          className={`cursor-pointer ${
+            activeLink === "/partnerships"
+              ? "text-green-500"
+              : "hover:text-green-500"
+          }`}
+          onClick={() => handleClick("/partnerships")}
         >
           Associations & Partnerships
         </a>
         <a
-          className="hover:text-green-500 cursor-pointer"
-          onClick={() => (window.location.href = "/contact")}
+          className={`cursor-pointer ${
+            activeLink === "/contact"
+              ? "text-green-500"
+              : "hover:text-green-500"
+          }`}
+          onClick={() => handleClick("/contact")}
         >
           Contact
         </a>
         <a
           className="hover:text-gray-800 bg-green-500 text-black font-bold p-3 rounded-full cursor-pointer"
-          onClick={() => (window.location.href = "/tenancy")}
+          onClick={() => handleClick("/tenancy")}
         >
           Pitch us
         </a>
