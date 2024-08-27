386d66f83e441c5455ffb9102d727421c7953d8c
Sohaib-Ahmed869 <i211114@nu.edu.pk>
2024-08-27 15:53:14 +0500

    responsive

--- src/About/page.js
+++ src/About/page.js
@@ -210,6 +210,38 @@ const PakistanAbout = () => {
   );
 };
 
+const PakistanAboutMobile = () => {
+  return (
+    <div>
+      <div className="flex flex-col lg:flex-row justify-between items-center lg:gap-20">
+        <img src={pakmap} alt="Pakistan Map" className="w-1/2 mb-5"></img>
+        <div className="lg:w-3/4">
+          <h1 className="mb-4 text-xl lg:text-3xl font-extrabold leading-none tracking-tight text-gray-900">
+            PAKISTAN -{" "}
+            <span className="text-green-400">
+              THE GLOBAL DESTINATION FOR LOVERS OF{" "}
+            </span>{" "}
+            NATURE, FOOD, HISTORY & HOSPITALITY
+          </h1>
+          <p className="text-sm lg:text-md font-normal text-gray-500 mb-10 leading-6 text-justify">
+            Pakistan is situated at the western periphery of South Asia and
+            ranks among the most significant members of the developing world. It
+            is a global destination for the admirers of nature's marvels and
+            beauty. From the highest peaks in the world like K2, to the mighty
+            Indus river; from mouth-watering cuisine to handcrafted pieces of
+            art, Pakistan offers an experience of a lifetime to all its
+            visitors. In the technological domain, Pakistan's teeming number of
+            scientists, engineers, doctors, and technicians promises the country
+            a bright future in science, technology, and innovation-based
+            progression. Our youth is brimming with ideas and is considered
+            among the most intelligent, creative and hardworking in the world.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const IslamabadAbout = () => {
   return (
     <div>
@@ -256,6 +288,41 @@ const IslamabadAbout = () => {
           ))}
         </div>
       }
+    </div>
+  );
+};
+
+const IslamabadAboutMobile = () => {
+  return (
+    <div>
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-20 mt-10">
+        <img src={islamabadpng} alt="Pakistan Map" className="w-1/2"></img>
+        <div className="lg:w-3/4">
+          <h1 className="mb-4 text-xl lg:text-3xl font-extrabold leading-none tracking-tight text-gray-900">
+            ISLAMABAD -{" "}
+            <span className="text-green-400">
+              PAKISTAN'S PREMIER LOCATION FOR{" "}
+            </span>{" "}
+            INNOVATIVE TECHNOLOGY-DRIVEN ENTERPRISES
+          </h1>
+          <p className="text-sm lg:text-md font-normal text-gray-500 mb-10 leading-6 text-justify">
+            Islamabad is the capital of Pakistan, and is one of the most
+            picturesque and beautiful capitals in the world, in addition to
+            being a safe and secure city. It is one of the most well planned and
+            well developed cities of Pakistan, and embodies man's marvel of
+            converting wilderness into remarkable pieces of estate. Islamabad is
+            home to settlers from all nooks and corners of the country, and is a
+            region-wide symbol of progress, cleanliness and urban development.
+            Islamabad is recognized as the metropolitan for culture, political
+            activities, education, and innovation, and the centre of ancient
+            civilizations dating back to the earliest Stone Age. With an
+            overwhelming majority of country's prolific research institutes,
+            universities and industries within and around a 30-mile radius of
+            the city, Islamabad is the most ideal location for any science and
+            technology initiatives.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
@@ -307,6 +374,38 @@ const NustAbout = () => {
   );
 };
 
+const NustAboutMobile = () => {
+  return (
+    <div>
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-20 mt-10">
+        <img src={nstppng} alt="Pakistan Map" className="w-full"></img>
+        <div className="lg:w-3/4">
+          <h1 className="mb-4 text-xl lg:text-3xl font-extrabold leading-none tracking-tight text-gray-900">
+            NATIONAL UNIVERSITY OF SCIENCES AND TECHNOLOGY -{" "}
+            <span className="text-green-400">
+              PAKISTAN'S PREMIER UNIVERSITY FOR SCIENCE AND TECHNOLOGY
+            </span>
+          </h1>
+          <p className="text-sm lg:text-md font-normal text-gray-500 mb-10 leading-6 text-justify">
+            National University of Sciences and Technology (NUST) is a
+            multi-school university with its headquarters in Islamabad,
+            Pakistan. The university offers undergraduate and graduate degrees,
+            including doctoral and professional degrees. The university is known
+            for its strong emphasis on science and technology, and its rigorous
+            academic programs. NUST is one of the top universities in Pakistan,
+            and is ranked among the top 400 universities in the world. The
+            university has a strong research program, and is home to several
+            research centers and institutes. NUST is also known for its
+            innovative teaching methods, and its focus on developing
+            well-rounded students who are prepared to succeed in the global
+            marketplace.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const NSTPAbout = () => {
   return (
     <div>
@@ -353,6 +452,41 @@ const NSTPAbout = () => {
   );
 };
 
+const NSTPAboutMobile = () => {
+  return (
+    <div>
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-20">
+        <img
+          src={nstplogocolored}
+          alt="Pakistan Map"
+          className="w-1/2 mb-5"
+        ></img>
+        <div className="lg:w-3/4">
+          <h1 className="mb-4 text-xl lg:text-3xl font-extrabold leading-none tracking-tight text-gray-900">
+            NATIONAL SCIENCE AND TECHNOLOGY PARK -{" "}
+            <span className="text-green-400">
+              PAKISTAN'S PREMIER TECHNOLOGY PARK FOR SCIENCE AND TECHNOLOGY
+            </span>
+          </h1>
+          <p className="text-sm lg:text-md font-normal text-gray-500 mb-10 leading-6 text-justify">
+            National Science and Technology Park (NSTP) is a technology park
+            located in Islamabad, Pakistan. The park is home to a number of
+            high-tech companies, startups, and international firms. The park is
+            designed to foster innovation and collaboration among its tenants,
+            and to provide them with the resources they need to succeed. NSTP is
+            also home to a number of research centers and institutes, and is
+            known for its strong research program. The park is also home to a
+            number of educational institutions, and is a hub for technology
+            education and training. NSTP is committed to promoting science and
+            technology in Pakistan, and to helping the country develop a strong,
+            knowledge-based economy.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const About = () => {
   const [isLaptop, setIsLaptop] = useState(false);
 
@@ -369,35 +503,43 @@ const About = () => {
     };
   }, []);
   return (
-    <div className="p-4 lg:p-20 no-scrollbar overflow-y-hidden">
-      <Swiper
-        direction="vertical"
-        slidesPerView={1}
-        mousewheel={{ thresholdDelta: 20 }}
-        className="mySwiper"
-        style={{ height: "100vh" }}
-      >
-        <SwiperSlide>
-          <div id="hero" className="h-screen flex items-center">
-            <PakistanAbout />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div id="who-we-are" className="h-screen flex items-center">
-            <IslamabadAbout />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div id="intro" className="h-screen flex items-center">
-            <NustAbout />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div id="intro" className="h-screen flex items-center">
-            <NSTPAbout />
-          </div>
-        </SwiperSlide>
-      </Swiper>
+    <div>
+      <div className="p-4 mt-36 lg:hidden">
+        <PakistanAboutMobile />
+        <IslamabadAboutMobile />
+        <NustAboutMobile />
+        <NSTPAboutMobile />
+      </div>
+      <div className="p-4 lg:p-20 no-scrollbar overflow-y-hidden hidden lg:block">
+        <Swiper
+          direction="vertical"
+          slidesPerView={1}
+          mousewheel={{ thresholdDelta: 20 }}
+          className="mySwiper"
+          style={{ height: "100vh" }}
+        >
+          <SwiperSlide>
+            <div id="hero" className="h-screen flex items-center">
+              <PakistanAbout />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div id="who-we-are" className="h-screen flex items-center">
+              <IslamabadAbout />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div id="intro" className="h-screen flex items-center">
+              <NustAbout />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div id="intro" className="h-screen flex items-center">
+              <NSTPAbout />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
